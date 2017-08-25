4166d03fba139e7539884f0421199adabf341f86
DXL <sishenyihuba@users.noreply.github.com>
2017-08-25 12:08:06 +0800

    initial commint
    Signed-off-by: DXL <sishenyihuba@users.noreply.github.com>

---
+++ ThreeRingControl/ThreeRingControl.h
@@ -0,0 +1,19 @@
+//
+//  ThreeRingControl.h
+//  ThreeRingControl
+//
+//  Created by macache on 2017/8/25.
+//  Copyright © 2017年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThreeRingControl.
+FOUNDATION_EXPORT double ThreeRingControlVersionNumber;
+
+//! Project version string for ThreeRingControl.
+FOUNDATION_EXPORT const unsigned char ThreeRingControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreeRingControl/PublicHeader.h>
+
+
